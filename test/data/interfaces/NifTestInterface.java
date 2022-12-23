83bd88a67edf7cd6ecd27951115fa0f68718a753
peremunoz <73690199+peremunoz@users.noreply.github.com>
2022-12-24 00:43:56 +0100

    Added @Test annotations for running the tests from the interface

--- test/data/interfaces/NifTestInterface.java
+++ test/data/interfaces/NifTestInterface.java
@@ -1,20 +1,30 @@
 package data.interfaces;
 
+import org.junit.jupiter.api.Test;
+
 public interface NifTestInterface {
 
+    @Test
     void getNifTest();
 
+    @Test
     void nullNifTest();
 
+    @Test
     void nifWithLessThanNineCharactersTest();
 
+    @Test
     void nifWithMoreThanNineCharactersTest();
-    
+
+    @Test
     void nifWithEightDigitsAndOneLowerCaseLetterTest();
 
+    @Test
     void nifWithEightDigitsAndOneNumberTest();
 
+    @Test
     void nifWithEightDigitsAndOneSpecialCharacterTest();
 
+    @Test
     void nifWithEightDigitsAndOneSpaceTest();
 }
