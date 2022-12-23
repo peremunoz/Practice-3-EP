6be4d9569ce9b26dae2162462a2fae925058186b
peremunoz <73690199+peremunoz@users.noreply.github.com>
2022-12-24 00:16:32 +0100

    Implemented class and some error control

---
+++ src/data/SmallCode.java
@@ -0,0 +1,41 @@
+package data;
+
+/**
+ * Essential data classes
+ */
+
+final public class SmallCode {
+
+    // Code composed of 3 digits. Used for the Safe Verification Code and for the Cl@ve system.
+
+    private final String code;
+
+    public SmallCode (String code) {
+        if (code == null) throw new IllegalArgumentException("Code cannot be null");
+        checkCode(code);
+        this.code = code;
+    }
+
+    private void checkCode (String code) {
+        if (code.length() != 3) throw new IllegalArgumentException("Code must have 3 characters");
+        if (!code.matches("[0-9]{3}")) throw new IllegalArgumentException("Code must have 3 digits");
+    }
+
+    public String getCode () { return code; }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmallCode smallCode = (SmallCode) o;
+        return code.equals(smallCode.code);
+    }
+
+    @Override
+    public int hashCode () { return code.hashCode(); }
+
+    @Override
+    public String toString () {
+        return "SmallCode{" + "code='" + code + '\'' + '}';
+    }
+}
