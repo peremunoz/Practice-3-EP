1726f2894d33dfbcb44bd5af3df17ebecec25589
peremunoz <73690199+peremunoz@users.noreply.github.com>
2022-12-24 00:22:26 +0100

    Implemented class and some error control

---
+++ src/data/DigitalSignature.java
@@ -0,0 +1,37 @@
+package data;
+
+import java.util.Arrays;
+
+/**
+ * Essential data classes
+ */
+
+final public class DigitalSignature {
+
+    // Digital signature for representing the electronic signature.
+
+    private final byte[] digitalSignature;
+
+    public DigitalSignature (byte[] digitalSignature) {
+        if (digitalSignature == null) throw new IllegalArgumentException("Digital signature cannot be null");
+        this.digitalSignature = digitalSignature;
+    }
+
+    public byte[] getDigitalSignature () { return digitalSignature; }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DigitalSignature digitalSignature1 = (DigitalSignature) o;
+        return Arrays.equals(digitalSignature, digitalSignature1.digitalSignature);
+    }
+
+    @Override
+    public int hashCode () { return Arrays.hashCode(digitalSignature); }
+
+    @Override
+    public String toString () {
+        return "DigitalSignature{" + "digitalSignature='" + Arrays.toString(digitalSignature) + '\'' + '}';
+    }
+}
